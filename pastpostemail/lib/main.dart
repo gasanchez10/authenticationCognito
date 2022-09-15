52e97371d8ecf5c8a4af17d9cb1d5ccbdebbd7d9
gasanchez10 <gsanchez@pastpost.com>
2022-09-15 15:20:51 -0500

    Added amplify

--- pastpostemail/lib/main.dart
+++ pastpostemail/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
+
+import 'amplifyconfiguration.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,15 +36,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,15 +45,104 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      final auth = AmplifyAuthCognito();
+      await Amplify.addPlugin(auth);
+
+      // call Amplify.configure to use the initialized categories in your app
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      print('An error occurred configuring Amplify: $e');
+    }
+  }
+
+  bool isSignUpComplete = false;
+
+  Future<void> signUpUser() async {
+    try {
+      final userAttributes = <CognitoUserAttributeKey, String>{
+        CognitoUserAttributeKey.email: 'gersan1997@hotmail.com',
+        CognitoUserAttributeKey.phoneNumber: '+573013382345',
+        // additional attributes as needed
+      };
+      final result = await Amplify.Auth.signUp(
+        username: 'germancitosegundo',
+        password: 'mysupersecurepassword',
+        options: CognitoSignUpOptions(userAttributes: userAttributes),
+      );
+      setState(() {
+        isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      print(e.message);
+    }
+  }
+
+  Future<void> confirmUser() async {
+    try {
+      final result = await Amplify.Auth.confirmSignUp(
+          username: 'germancitosegundo', confirmationCode: '567605');
+
+      setState(() {
+        isSignUpComplete = result.isSignUpComplete;
+      });
+    } on AuthException catch (e) {
+      print(e.message);
+    }
+  }
+
+// Create a boolean for checking the sign in status
+  bool isSignedIn = false;
+
+  Future<void> signInUser(String username, String password) async {
+    try {
+      final result = await Amplify.Auth.signIn(
+        username: username,
+        password: password,
+      );
+      print('Resultado');
+      print(result.nextStep);
+      fetchAuthSession();
+      setState(() {
+        isSignedIn = result.isSignedIn;
+      });
+    } on AuthException catch (e) {
+      print('Hubo un error wntrando');
+      print(e.message);
+    }
+  }
+
+  Future<void> fetchAuthSession() async {
+    try {
+      final result = await Amplify.Auth.fetchAuthSession(
+        options: CognitoSessionOptions(getAWSCredentials: true),
+      );
+      String identityId = (result as CognitoAuthSession).identityId!;
+      print('identityId: $identityId');
+    } on AuthException catch (e) {
+      print(e.message);
+    }
+  }
+
+  Future<void> signOutUser() async {
+    try {
+      final result = await Amplify.Auth.signOut();
+      setState(() {
+        print('Result');
+        print(result);
+        isSignedIn = !isSignedIn;
+      });
+    } on AuthException catch (e) {
+      print('Hubo un error wntrando');
+      print(e.message);
+    }
   }
 
   @override
@@ -69,47 +153,30 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: Column(
+        children: [
+          TextButton(
+            onPressed: () async => {signUpUser()},
+            child: Text('TextButton'),
+          ),
+          TextButton(
+            onPressed: () async =>
+                {signInUser('germancitosegundo', 'mysupersecurepassword')},
+            child: Text('Sign in'),
+          ),
+          TextButton(
+            onPressed: () async => {signOutUser()},
+            child: Text('Sign out'),
+          ),
+          TextButton(
+            onPressed: () async => {confirmUser()},
+            child: Text('Confirm'),
+          ),
+        ],
+      ))),
     );
   }
 }
